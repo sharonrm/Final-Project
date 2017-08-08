b9fddc18df95434e76d2dec4d28566716879ade2
Sharon Moskal <sharonmoskal@icloud.com>
2017-08-08 12:15:03 -0400

    getting data on backend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-'use strict';
+// 'use strict';
+console.log('runnin!!')
 
 const express = require('express'),
 	  expressBrowserify = require('express-browserify'),
@@ -8,7 +9,10 @@ const express = require('express'),
 	  bodyParser = require('body-parser'),
 	  cors = require('cors'),
 	  app	= express(),
-	  PORT = process.env.PORT || 8080;
+	  PORT = process.env.PORT || 8080,
+    axios = require('axios'),
+    ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
 
 const isDev = app.get('env') === 'development';
 app.get(
@@ -40,22 +44,71 @@ dotenv.load({ silent: true });
 // 3. connect controller
 app.use('/api', require('./controllers/index'));
 
-// For local development, specify the username and password or set env properties
-const ltAuthService = new watson.AuthorizationV1({
-  username: process.env.SERVICE_NAME_USERNAME || 'efb6eaff-4a08-4e24-b5b1-9eb6e3da9fd8',
-  password: process.env.SERVICE_NAME_PASSWORD || 'oP5YW0t5iYcG',
-  url: watson.ToneAnalyzerV3.URL
-});
 
-app.get('/api/token/tone_analyzer', function(req, res) {
-  ltAuthService.getToken(function(err, token) {
-    if (err) {
-      console.log('Error retrieving token: ', err);
-      return res.status(500).send('Error retrieving token');
-    }
-    res.send(token);
+let emailStuff = "How many of us can say we wore our wedding dress, after our wedding? Creating deliberate pieces that work harmoniously with the fluctuations your wardrobe has to deal with is the type of slow fashion we believe in."
+// The following code came directly from the Watson API docs:
+const getWatsonTone = (inputEmail) => {
+    var tone_analyzer = new ToneAnalyzerV3({
+    username: process.env.SERVICE_NAME_USERNAME,
+    password: process.env.SERVICE_NAME_PASSWORD,
+    version_date: '2016-05-19'
   });
-});
 
-// 4. listen
+  var params = {
+    // Get the text from the JSON file.
+    text: inputEmail,
+    tones: 'emotion'
+  };
+
+  tone_analyzer.tone(params, function(error, response) {
+    if (error) {
+      console.log('error:', error);
+    } else {
+      console.log('!!!!!!')
+      console.log(JSON.stringify(response, null, 2));
+    }
+    }
+  );
+}
+
+getWatsonTone(emailStuff);
+
+
+// For local development, specify the username and password or set env properties
+// const ltAuthService = new watson.AuthorizationV1({
+//   username: process.env.SERVICE_NAME_USERNAME || 'efb6eaff-4a08-4e24-b5b1-9eb6e3da9fd8',
+//   password: process.env.SERVICE_NAME_PASSWORD || 'oP5YW0t5iYcG',
+//   url: watson.ToneAnalyzerV3.URL
+// });
+
+// app.get('/api/token/tone_analyzer', function(req, res) {
+//   ltAuthService.getToken(function(err, token) {
+//     if (err) {
+//       console.log('Error retrieving token: ', err);
+//       return res.status(500).send('Error retrieving token');
+//     }
+//     res.send(token);
+//   });
+// });
+     
+
+// axios({
+//   method: 'post',
+//   url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&sentences=false',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Authorization": "Basic MzE3N2U3YzAtMDU2Yi00YzExLThiYjYtNzgxNGJiNzAwNmRkOldiNTNKRlFYV052Zg=="
+//   },
+//   data: {
+//     "text":"How many of us can say we wore our wedding dress, after our wedding? Creating deliberate pieces that work harmoniously with the fluctuations your wardrobe has to deal with is the type of slow fashion we believe in."
+//   }
+// }).then((response) => {
+//   console.log('!!!!!!!')
+//   console.log(response.data)
+// }).catch((err) => {
+//   console.log(err)
+// })
+  
+
+// // 4. listen
 app.listen(PORT, () => console.log('Server is listening on port: ', PORT));
